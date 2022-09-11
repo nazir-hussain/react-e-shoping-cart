1b25b4c5830e995464a51cb6bbd7809139a273c2
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-09-11 19:36:02 +0530

    Add files via upload

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './component/Navbar';
+import Shop from './component/Shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Shop />
+    </div>
+  );
+}
+
+export default App;
