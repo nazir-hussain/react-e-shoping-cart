54d6967f5bf6d5b91fa0087344a24c7c483bcbf6
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-09-13 18:08:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import './App.css';
-import Navbar from './component/Navbar';
-import Shop from './component/Shop';
+import React, { useState,} from "react";
+import Shop from "./component/Shop";
+import Navbar from "./component/Navbar";
+import Cart from "./component/Cart";
+import Footer from "./component/Footer";
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
+
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
+
+  // useEffect(() => {
+  //   console.log("cart change");
+  // }, [cart]);
+
   return (
-    <div className="App">
-      <Navbar />
-      <Shop />
-    </div>
+    <React.Fragment>
+      <Navbar setShow={setShow} size={cart.length} />
+      {show ? (
+        <Shop handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
+      <Footer />
+    </React.Fragment>
   );
-}
+};
 
 export default App;
